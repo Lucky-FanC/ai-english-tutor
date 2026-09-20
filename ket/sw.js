e75b8f0a54d50b229abda8f2c8f218dfd3738000
Lucky-FanC <fanxiaoc@163.com>
2026-09-20 10:20:34 +0800

    Deploy ket: sw.js

---
+++ ket/sw.js
@@ -0,0 +1,14 @@
+/* KET 默写小助手 Service Worker：整站离线缓存 */
+const CACHE = 'ket-v1';
+const ASSETS = ['./', 'index.html', 'style.css', 'app.js', 'data-ket.js', 'manifest.json', 'icon.svg'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys()
+    .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
